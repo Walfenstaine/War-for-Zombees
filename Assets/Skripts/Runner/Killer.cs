f795898ee5993c8fd383346b798df70ecebb1d3a
Walfenstaine <Pavlik-gureev@mail.ru>
2023-05-08 20:31:39 +0300

    65678

--- Assets/Skripts/Runner/Killer.cs
+++ Assets/Skripts/Runner/Killer.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Killer : MonoBehaviour
 {
+    public AudioClip clip;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Interface iF = FindObjectOfType<Interface>() as Interface;
+            SoundPlayer.regit.sorse.Stop();
+            SoundPlayer.regit.sorse.PlayOneShot(clip);
             iF.Ded();
         }
     }
