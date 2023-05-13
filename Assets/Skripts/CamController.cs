5a038abe63afeaae37fb6d15bd10048dad12c6ff
Walfenstaine <Pavlik-gureev@mail.ru>
2023-05-13 19:44:19 +0300

    111

--- Assets/Skripts/CamController.cs
+++ Assets/Skripts/CamController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CamController : MonoBehaviour
 {
+    public float intensive = 2.0f;
     public Interface iF;
     public CharacterController player;
     void Update()
     {
         if (player != null)
         {
-            transform.position = Vector3.Lerp(transform.position, player.transform.position+player.velocity, 3.5f * Time.deltaTime);
+            Vector3 pos = player.transform.position + player.velocity;
+            transform.position = Vector3.Lerp(transform.position, pos, intensive * Time.deltaTime);
         }
         else {
             iF.Ded();
