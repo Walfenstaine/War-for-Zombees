b6806f15ba4f3ba9c012199c016db0d392c5d36a
Walfenstaine <Pavlik-gureev@mail.ru>
2023-05-28 12:05:04 +0300

    112321234

--- Assets/Skripts/AddCoins.cs
+++ Assets/Skripts/AddCoins.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddCoins : MonoBehaviour
 {
+    public Text text;
     public Data data;
     public AudioClip clip;
 
     public void Addcoins()
     {
         SoundPlayer.regit.sorse.PlayOneShot(clip);
-        data.coins += 100;
+        data.coins += data.record * 100;
         SaveAndLoad.Instance.Save();
+    }
+    private void Update()
+    {
+        text.text = "+" + data.record * 100;
     }
 }
